2ad532f23a8b7ef34b6e2126f193b81778153362
Mariia D <mariia.demidova0304@gmail.com>
2025-06-26 23:16:51 -0400

    Added event lisneter for updated quantity, total cost, set up alerts

--- script.js
+++ script.js
@@ -9,13 +9,26 @@ let totalPrice = 0;
 //Event listener to add an item
 addProductButton.addEventListener(`click`, () => {
     let newItem = document.createElement(`li`);
-    let productName = productNameInput.value;
-    let capitalizedName = productName.charAt(0).toUpperCase() + productName.slice(1);
-    let productPrice = parseFloat(productPriceInput.value);
-    clearInput();
-    newItem.innerHTML = `<span>${capitalizedName}</span> <span>Price: $${productPrice}</span><input type="number" class="quantity" value="1"/> <button class="remove">Remove Item</button>`;
-    cart.appendChild(newItem);
-    updateTotalPrice(productPrice);
+    //checking if name and price are valid inputs
+    if (productNameInput.value === ``) {
+        alert(`Please, put in the name of the product!`);
+    } else if (productPriceInput.value === ``) {
+        alert(`Please, put in price!`);
+    } else {
+        let productPrice = parseFloat(productPriceInput.value);
+        if (productPrice <= 0) {
+            alert(`Please, put in valid price!`);
+        } else {
+            let productName = productNameInput.value;
+            let capitalizedName = productName.charAt(0).toUpperCase() + productName.slice(1);
+            newItem.dataset.price = productPrice;
+            newItem.dataset.quantity = 1;
+            newItem.innerHTML = `<span>${capitalizedName}</span> <span>Price: $${productPrice}</span><input type="number" class="quantity" value="1"/> <button class="remove">Remove Item</button>`;
+            cart.appendChild(newItem);
+            updateTotalPrice(productPrice);
+            clearInput();
+        }
+    }
 })
 
 //delegated event listener to add removing function
@@ -25,6 +38,19 @@ cart.addEventListener(`click`, (event) => {
     }
 })
 
+//event listener for quantity change and updating total cost
+cart.addEventListener(`change`, (event) => {
+    if (event.target.classList.contains(`quantity`)) {
+        const newCount = parseFloat(event.target.value);
+        const item = event.target.closest(`li`);
+       const oldCount = parseFloat(item.dataset.quantity);
+       const price = parseFloat(item.dataset.price);
+       item.dataset.quantity = newCount;
+       let differenceCount = newCount-oldCount;
+        const differencePrice = price*differenceCount;
+        updateTotalPrice(differencePrice);
+    }
+})
 
 // Function to update the total price
 function updateTotalPrice(amount) {
@@ -35,8 +61,8 @@ function updateTotalPrice(amount) {
 // Function to remove an item
 function removeItem(event) {
     const item = event.target.closest('li');
-    //   const price = parseFloat(item.dataset.price);
-    //   updateTotalPrice(-price);
+    const price = parseFloat(item.dataset.price);
+    updateTotalPrice(-price);
     item.remove();
 }
 
@@ -44,3 +70,4 @@ function clearInput() {
     productNameInput.value = ``;
     productPriceInput.value = ``;
 }
+
